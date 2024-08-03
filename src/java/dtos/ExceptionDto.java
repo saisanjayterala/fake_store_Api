d93239e92104b3fd2b3dc730ebb2d5f3438f40ad
saisanjayterala <saisanjayterala@gmail.com>
2024-08-03 11:14:55 +0000

    2nd v

---
+++ src/java/dtos/ExceptionDto.java
@@ -0,0 +1,15 @@
+package dev.sanjay.dtos;
+
+public class ExceptionDto {
+    private String message;
+
+    // Getters and setters
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
